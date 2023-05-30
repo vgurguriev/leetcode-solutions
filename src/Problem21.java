b54040aba34f13163564d232b32cfa0394bde166
Veselin Gurguriev <veselin.gurguriev@sirma.bg>
2023-05-30 17:13:11 +0300

    21. Merge Two Sorted Lists

---
+++ src/Problem21.java
@@ -0,0 +1,50 @@
+/*
+    You are given the heads of two sorted linked lists list1 and list2.
+
+    Merge the two lists in a one sorted list.
+
+    The list should be made by splicing together the nodes of the first two lists.
+
+    Return the head of the merged linked list.
+     */
+
+public class Problem21 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (a != null && b != null) {
+            if (a.val <= b.val) {
+                tail.next = a;
+                a = a.next;
+            } else {
+                tail.next = b;
+                b = b.next;
+            }
+
+            tail = tail.next;
+        }
+
+        if (a == null) {
+            tail.next = b;
+        } else {
+            tail.next = a;
+        }
+
+        return dummy.next;
+    }
+}
