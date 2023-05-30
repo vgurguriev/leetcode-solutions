0c336351e385b3da7a8b88e9a113d9181aa90e4e
Veselin Gurguriev <veselin.gurguriev@sirma.bg>
2023-05-30 17:07:55 +0300

    1. Two Sum

--- src/Problem01.java
+++ src/Problem01.java
@@ -1,4 +1,15 @@
 public class Problem01 {
+    /*
+    Given an array of integers nums and an integer target,
+
+    return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution,
+
+    and you may not use the same element twice.
+
+    You can return the answer in any order.
+    */
     public static int[] twoSum(int[] nums, int target) {
 
         for (int i = 0; i < nums.length - 1; i++) {
